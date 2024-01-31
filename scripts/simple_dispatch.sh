e6720188bf46447cdea0919cfa6962a81374a219
Mehrdad Faghih-Ordobadi <mehrdadfaghih-ordobadi@Mehrdads-MacBook-Pro-2.local>
2024-01-31 15:59:43 -0800

    fixed typo in testing.yml

--- scripts/simple_dispatch.sh
+++ scripts/simple_dispatch.sh
@@ -1,7 +1,7 @@
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer 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" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/mehrdad-ordobadi/melo-3.0/actions/workflows/testing.yml/dispatches \
   -d '{"ref":"testing","inputs":{}'

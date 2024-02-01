dd81bcf97362617e97347d333eba4a875eaf5456
Mehrdad Faghih-Ordobadi <mehrdadfaghih-ordobadi@Mehrdads-MacBook-Pro-2.local>
2024-01-31 16:29:30 -0800

    yeahhh

--- scripts/simple_dispatch.sh
+++ scripts/simple_dispatch.sh
@@ -1,7 +1,7 @@
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer *********" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/mehrdad-ordobadi/melo-3.0/actions/workflows/testing.yml/dispatches \
   -d '{"ref":"testing","inputs":{}'
